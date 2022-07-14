14f3ee7ded1883052531c0c56a2778f54f831c58
Guillermo Piñate <gpinate@gmail.com>
2022-07-14 11:57:48 +0200

    base de datos creada y cargada de datos ancor home , list hecha

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,58 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\Apartment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route("/apartment")]
-    public function getApartment()
+    #[Route("/apartment/{id}", name: "showApartment")]
+    public function getApartment($id, EntityManagerInterface $doctrine)
     {
-        $apartment = [
-            "name" => "Piso Calle Oporto, Vigo.",
-            "address" => "Calle Oporto, Vigo. Pontevedra.",
-            "description" => "Piso de 3 habitaciones y dos Baños 105 mt2.",
-            "price" => "850,00 €",
-            "image" => "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80"
-        ];
+        $repository = $doctrine->getRepository(Apartment::class);
+        $apartment = $repository->find($id);
         return $this->render("apartment/showapartment.html.twig", ['apartment' => $apartment]);
     }
 
 
-    #[Route("/apartments")]
-    public function getApartments()
+    #[Route("/apartments", name: "getApartments")]
+    public function getApartments(EntityManagerInterface $doctrine)
     {
-        $apartments = [
-            [
-                "name" => "Piso Calle Oporto, Vigo.",
-                "address" => "Calle Oporto Numero 1 5to Derecha, Vigo. Pontevedra.",
-                "description" => "Piso de 3 habitaciones y dos Baños 105 mt2.",
-                "price" => "850,00 €",
-                "image" => "https://images.unsplash.com/photo-1597047084897-51e81819a499?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
-            ],
-            [
-                "name" => "Casa Calle Gran vía, Vigo.",
-                "address" => "Calle Gran vía, Vigo.",
-                "description" => "Casa de 3 habitaciones y dos Baños 280 mt2.",
-                "price" => "2800,00 €",
-                "image" => "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1475&q=80"
-            ],
-            [
-                "name" => "Piso Calle Placer, Vigo.",
-                "address" => "Calle Placer, Vigo. Pontevedra.",
-                "description" => "Piso de 3 habitaciones y dos Baños 150 mt2.",
-                "price" => "950,00 €",
-                "image" => "https://www.inmoserver.com/fotos/0487/wm/2072_o_1f381hfug13u09ib6l01fme6t1g.jpg"
-            ]
-
-        ];
+        $repository = $doctrine->getRepository(Apartment::class);
+        $apartments = $repository->findAll();
         return $this->render("apartment/apartmentList.html.twig", ["apartments" => $apartments]);
     }
 
-    #[Route("/home")]
+    #[Route("/home", name: "home")]
     public function getHome()
     {
         return $this->render("home/home.html.twig");
+    }
+
+    #[Route("/insert/apartment")]
+    public function insertApartment(EntityManagerInterface $doctrine)
+    {
+        $apartment1 = new Apartment();
+        $apartment1->setName("Piso Calle Oporto, Vigo.");
+        $apartment1->setAddress("Calle Oporto Numero 1 5to Derecha, Vigo. Pontevedra.");
+        $apartment1->setDescription("Piso de 3 habitaciones y dos Baños 105 mt2.");
+        $apartment1->setPrice("850,00 €");
+        $apartment1->setImage("https://images.unsplash.com/photo-1597047084897-51e81819a499?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80");
+
+        $apartment2 = new Apartment();
+        $apartment2->setName("Casa Calle Gran vía, Vigo.");
+        $apartment2->setAddress("Calle Gran vía, Vigo.");
+        $apartment2->setDescription("Casa de 5 habitaciones y 3 Baños 280 mt2.");
+        $apartment2->setPrice("2800,00 €");
+        $apartment2->setImage("https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1475&q=80");
+
+        $apartment3 = new Apartment();
+        $apartment3->setName("Piso Calle Placer, Vigo.");
+        $apartment3->setAddress("Calle Placer, Vigo. Pontevedra.");
+        $apartment3->setDescription("Piso de 3 habitaciones y dos Baños 150 mt2.");
+        $apartment3->setPrice("950,00 €");
+        $apartment3->setImage("https://www.inmoserver.com/fotos/0487/wm/2072_o_1f381hfug13u09ib6l01fme6t1g.jpg");
+
+        $doctrine->persist($apartment1);
+        $doctrine->persist($apartment2);
+        $doctrine->persist($apartment3);
+        $doctrine->flush();
+        return new Response("Vivienda insertada correctamente");
     }
 }
